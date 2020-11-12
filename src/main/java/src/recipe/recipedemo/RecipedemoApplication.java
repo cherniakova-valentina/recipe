cc3ebaaa4c79ae60be6da7dad680914f1d852be6
v.cherniakov <valushechka1996@gmail.com>
2020-11-12 10:29:50 +0200

    Init commit

---
+++ src/main/java/src/recipe/recipedemo/RecipedemoApplication.java
@@ -0,0 +1,13 @@
+package src.recipe.recipedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipedemoApplication.class, args);
+	}
+
+}

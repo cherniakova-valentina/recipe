7c6fe2777b87af00fd079dbedc410cd3b1d93dc3
Valentyna Cherniakova <v.cherniakov@samsung.com>
2022-02-04 10:50:06 +0200

    add create new recipe

--- src/main/java/src/recipe/recipedemo/controllers/RecipeController.java
+++ src/main/java/src/recipe/recipedemo/controllers/RecipeController.java
@@ -2,8 +2,8 @@ package src.recipe.recipedemo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import src.recipe.recipedemo.commands.RecipeCommand;
 import src.recipe.recipedemo.services.RecipeService;
 
 @Controller
@@ -14,10 +14,25 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 }
